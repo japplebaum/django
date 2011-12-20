b311c10c5ec1cd8bc30a5deafd1625558c46ff7e
japplebaum <julianapplebaum@gmail.com>
2011-12-20 10:41:48 -0500

    Everyone's merged now

--- django/shortcuts/__init__.py
+++ django/shortcuts/__init__.py
@@ -100,8 +100,8 @@ def _get_queryset(klass):
 
 def get_object_or_404(klass, *args, **kwargs):
     """
-    Uses get() to return an object, or raises a Http404 exception if the object
-    does not exist.
+    Uses get() to return an object, or raises a Http404 exception if the
+    object does not exist.
 
     klass may be a Model, Manager, or QuerySet object. All other passed
     arguments and keyword arguments are used in the get() query.
